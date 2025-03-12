31a69d226913dcdc6e5e072ca1ff2509ff93455f
Krishnaraj-Irkal <krishnarajirkal@gmail.com>
2025-03-13 00:31:46 +0530

    Added and fixed hero section, and nav bar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/cfe8xso.css" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
